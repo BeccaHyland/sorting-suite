c38105412817619426692fae4f487651523314f7
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-23 14:29:33 -0700

    functional bubble sort for array of 5 unduplicated unsorted numbers

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -9,5 +9,18 @@ describe('bubble sort class', function(){
   it('should instantiate a bubble sort', function(){
     var bubble = new BubbleSort;
     assert.isObject(bubble);
+  });
+
+  it('should have an unsorted array', function(){
+    var ua = [3,1,2];
+    var bubble = new BubbleSort(ua);
+    assert.equal(bubble.uA, ua);
+  })
+
+  it('should sort an array of numbers low to high', function(){
+    var unsortedArray = [4,3,1,2,5];
+    var sortedArray = [1,2,3,4,5];
+    var bubble = new BubbleSort(unsortedArray);
+    assert.deepEqual(bubble.sortArray(), sortedArray);
   })
 })
