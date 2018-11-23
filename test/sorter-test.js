eac2d1299a9d577302bad14a99222618ae68d28a
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-23 14:49:27 -0700

    plural

--- test/sorter-test.js
+++ test/sorter-test.js
@@ -2,34 +2,35 @@ const assert = require('chai').assert
 const Sorter = require('../lib/sorter')
 
 describe('sorter class', function(){
-  it('should be a function', function(){
-    assert.isFunction(Sorter);
-  });
+  context('bubble sorts', function(){
+    it('should be a function', function(){
+      assert.isFunction(Sorter);
+    });
 
-  it('can instantiate a sorter', function(){
-    var sorter = new Sorter;
-    assert.isObject(sorter);
-  });
+    it('can instantiate a sorter', function(){
+      var sorter = new Sorter;
+      assert.isObject(sorter);
+    });
 
-  it('can sort an array of numbers low to high', function(){
-    var unsortedArray = [4,3,1,5,2];
-    var sortedArray = [1,2,3,4,5];
-    var sorter = new Sorter;
-    assert.deepEqual(sorter.bubbleSortAsc(unsortedArray), sortedArray);
+    it('can sort an array of numbers low to high', function(){
+      var unsortedArray = [4,3,1,5,2];
+      var sortedArray = [1,2,3,4,5];
+      var sorter = new Sorter;
+      assert.deepEqual(sorter.bubbleSortAsc(unsortedArray), sortedArray);
+    })
+
+    it('can sort an array of numbers high to low', function(){
+      var unsortedArray = [4,3,1,5,2];
+      var sortedArray = [5,4,3,2,1];
+      var sorter = new Sorter;
+      assert.deepEqual(sorter.bubbleSortDesc(unsortedArray), sortedArray);
+    })
+
+    it('can sort an array of numbers low to high including duplicates', function(){
+      var unsortedArray = [4,4,1,5,2];
+      var sortedArray = [1,2,4,4,5];
+      var sorter = new Sorter;
+      assert.deepEqual(sorter.bubbleSortAsc(unsortedArray), sortedArray);
+    })
   })
-
-  it('can sort an array of numbers high to low', function(){
-    var unsortedArray = [4,3,1,5,2];
-    var sortedArray = [5,4,3,2,1];
-    var sorter = new Sorter;
-    assert.deepEqual(sorter.bubbleSortDesc(unsortedArray), sortedArray);
-  })
-
-  it('can sort an array of numbers low to high including duplicates', function(){
-    var unsortedArray = [4,4,1,5,2];
-    var sortedArray = [1,2,4,4,5];
-    var sorter = new Sorter;
-    assert.deepEqual(sorter.bubbleSortAsc(unsortedArray), sortedArray);
-  })
-
 })
