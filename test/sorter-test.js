cb2e8e88230379133e29874c0b0eef242163af30
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-23 14:45:24 -0700

    add test to ensure duplicate values are sorted

--- test/sorter-test.js
+++ test/sorter-test.js
@@ -6,22 +6,30 @@ describe('sorter class', function(){
     assert.isFunction(Sorter);
   });
 
-  it('should instantiate a sorter', function(){
+  it('can instantiate a sorter', function(){
     var sorter = new Sorter;
     assert.isObject(sorter);
   });
 
-  it('should sort an array of numbers low to high', function(){
-    var unsortedArray = [4,3,1,2,5];
+  it('can sort an array of numbers low to high', function(){
+    var unsortedArray = [4,3,1,5,2];
     var sortedArray = [1,2,3,4,5];
     var sorter = new Sorter;
     assert.deepEqual(sorter.bubbleSortAsc(unsortedArray), sortedArray);
   })
 
-  it('should sort an array of numbers high to low', function(){
-    var unsortedArray = [4,3,1,2,5];
+  it('can sort an array of numbers high to low', function(){
+    var unsortedArray = [4,3,1,5,2];
     var sortedArray = [5,4,3,2,1];
     var sorter = new Sorter;
     assert.deepEqual(sorter.bubbleSortDesc(unsortedArray), sortedArray);
   })
+
+  it('can sort an array of numbers low to high including duplicates', function(){
+    var unsortedArray = [4,4,1,5,2];
+    var sortedArray = [1,2,4,4,5];
+    var sorter = new Sorter;
+    assert.deepEqual(sorter.bubbleSortAsc(unsortedArray), sortedArray);
+  })
+
 })
